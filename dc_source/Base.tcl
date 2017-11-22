3fd6aacafba18a64f96e9f3feefc45d5afcbfbbe
Vladislav Zavjalov <slazav@altlinux.org>
2017-11-22 14:28:26 +0200

    fix comment

--- dc_source/Base.tcl
+++ dc_source/Base.tcl
@@ -37,6 +37,6 @@ namespace eval device_role::dc_source {
     # methods which should be defined by driver:
     method set_volt      {val} {}; # set voltage and all output settings
     method set_volt_fast {val} {}; # set voltage without touching other settings
-    method get_volt {} {};    # measure actual value of current
+    method get_volt {} {};    # measure actual voltage value
   }
 }
