cde840e73425323bfa75815226123f80f5b68548
Vladislav Zavjalov <slazav@altlinux.org>
2019-12-17 15:20:29 +0000

    gauge: support for Keysight/Agilent/HP multiplexer 34972A

--- r_gauge.tcl
+++ r_gauge.tcl
@@ -199,6 +199,85 @@ itcl::class keithley_nanov {
 }
 
 ######################################################################
+# Use Keysight/Agilent/HP multiplexer 34972A as a gauge device.
+#
+# ID strings:
+#   Agilent Technologies,34972A,MY59000704,1.17-1.12-02-02
+#
+# Channels: space-separated list of channel settings:
+#   ACI(101-102) DCV(103-104),etc.
+# Channel numbers are set as in the device: comma-separated lists: 101,105
+# or colon-separated ranges 101:105.
+# Valid prefixes are: DCI, ACV, DCV, R2, R4
+# For R4 measurement channels are pairs with n+10 (34901A extension or
+# n+8 (34902A extension)
+
+itcl::class keysight_mplex {
+  inherit interface
+  proc test_id {id} {
+    if {[regexp {,34972A,} $id]} {return {34972A}}
+    return {}
+  }
+
+  variable cmds {};  # list of measurement commands
+
+  constructor {d ch id} {
+    set dev $d
+
+    foreach c [split $ch { +}] {
+      if {[regexp {^([A-Z0-9]+)\(([0-9:,]+)\)$} $c v0 vmeas vch]} {
+
+        switch -exact -- $vmeas {
+          DCV {  lappend cmds "meas:volt:dc? (@$vch)" }
+          ACV {  lappend cmds "meas:volt:ac? (@$vch)" }
+          DCI {  lappend cmds "meas:curr:dc? (@$vch)" }
+          ACI {  lappend cmds "meas:curr:ac? (@$vch)" }
+          R2  {  lappend cmds "meas:res? (@$vch)"     }
+          R4  {  lappend cmds "meas:fres? (@$vch)"    }
+          default {
+            error "$this: bad channel setting: $c"
+            return
+          }
+        }
+
+      } else {
+        error "bad channel setting: $c"
+      }
+    }
+  }
+
+  ############################
+  method get {} {
+    set ret {}
+    foreach c $cmds { lappend ret [$dev cmd $c] }
+    return $ret
+  }
+  method get_auto {} {
+    return [get]
+  }
+
+  ############################
+  method list_ranges {} {
+  }
+  method list_tconsts {} {
+  }
+
+  ############################
+  method set_range  {val} {
+  }
+  method set_tconst {val} {
+  }
+
+  ############################
+  method get_range  {} {
+  }
+  method get_tconst {} {
+  }
+
+}
+
+
+######################################################################
 # Use Lockin SR844 as a gauge.
 #
 # ID string:
