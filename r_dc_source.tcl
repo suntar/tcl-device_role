33df5dc94784504e73e5f78c9bce42e5848f2147
Vladislav Zavjalov <slazav@altlinux.org>
2019-12-23 20:09:09 +0000

    dc_source: add LakeShore 370 heater output

--- r_dc_source.tcl
+++ r_dc_source.tcl
@@ -77,7 +77,77 @@ itcl::class keysight {
     return [$dev cmd "${sour_pref}VOLT:OFFS? "]
   }
 }
+######################################################################
+# Use LakeShore 370 heater output as a dc source.
+# The device is a current source. You set up heater resistior in
+# the channel setting and control voltage on the heater.
+#
+# ID string:
+#   LSCI,MODEL370,370A5K,04102008
+#
+# Use channel setting: HTR<range>:<heater_resistance_ohm>
+#   range:
+#   1 - 31.6 uA
+#   2 - 100 uA
+#   3 - 316 uA
+#   4 - 1 mA
+#   5 - 3.16 mA
+#   6 - 10 mA
+#   7 - 31.6 mA
+#   8 - 100 mA
 
+itcl::class LSCI370 {
+  inherit interface
+
+  proc test_id {id} {
+    if {[regexp {LSCI,MODEL370} $id]} {return 1}
+  }
+
+  variable chan;  # channel to use (1..2)
+  variable res; # heater resistance
+  variable rng; # heater curent range, A
+
+  constructor {d ch id} {
+    if {![regexp {^HTR([1-8]):([0-9e.]+)$} $ch v rng_num res]} {
+      error "$this: bad channel setting: $ch"}
+
+    set dev $d
+
+    switch -exact -- $rng_num {
+      1 {set rng 3.16e-5}
+      2 {set rng 1.00e-4}
+      3 {set rng 3.16e-4}
+      4 {set rng 1.00e-3}
+      5 {set rng 3.16e-3}
+      6 {set rng 1.00e-2}
+      7 {set rng 3.16e-2}
+      8 {set rng 1.00e-1}
+    }
+
+    set min_v 0
+    set max_v [expr $rng*$res]
+    set min_v_step [expr 1e-6*$max_v]
+
+    $dev cmd HTRRNG $rng_num
+    $dev cmd CMODE  3
+  }
+  method set_volt {volt} {
+    # calculate heater output
+    set v [expr 100*$volt/$max_v]
+    if {$v > 100} {set v 100}
+    if {$v < 0} {set v 0}
+    $dev cmd "MOUT $v"
+  }
+  method off {} {
+    set_volt 0
+  }
+  method get_volt {} {
+    set v [$dev cmd "MOUT?"]
+    return [expr $v*$max_v/100]
+  }
+}
+
+######################################################################
 # Use Lockin SR844 auxilary outputs as a voltage_suply.
 #
 # ID string:
